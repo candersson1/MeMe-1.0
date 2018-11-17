f4ebe2ecb01dd45b912f420863286597bd395d4d
Chris Andersson <candersson@klarsolutions.com>
2018-11-17 14:35:47 -0500

    Initial Commit

---
+++ image_picker_module/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  image_picker_module
+//
+//  Created by Chris Andersson on 2018-11-17.
+//  Copyright © 2018 klarsolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
